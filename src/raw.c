e47dea14651a4ba2e091b7e12b20e92d0ee654c2
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-05 21:47:32 -0300

    remove output processing

--- src/raw.c
+++ src/raw.c
@@ -14,6 +14,7 @@ void raw(void) {
   struct termios raw = orig_termios;
 
   raw.c_iflag &= ~(IXON | ICRNL);
+  raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
