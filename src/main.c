072a41248a723f27d70921d1fe986e887a277dae
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-05 21:54:22 -0300

    add timeout for read

--- src/main.c
+++ src/main.c
@@ -6,14 +6,16 @@
 int main() {
   raw();
 
-  char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while(1) {
+    char c;
+    read(STDIN_FILENO, &c, 1); 
     if(iscntrl(c)) {
       printf("%d\r\n", c);
     }
     else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if(c == 'q') break;
   }
 
   return 0;
--- src/raw.c
+++ src/raw.c
@@ -13,9 +13,14 @@ void raw(void) {
   atexit(cbreak);
   struct termios raw = orig_termios;
 
-  raw.c_iflag &= ~(IXON | ICRNL);
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+  //Timeout for read
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
