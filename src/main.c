4c69cb58ab64bc99023d0480cf97dcac153e5028
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-05 21:36:54 -0300

    display key pressed

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 #include "raw.h"
+#include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 
 int main() {
   raw();
 
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if(iscntrl(c)) {
+      printf("%d\n", c);
+    }
+    else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
 
   return 0;
 }
