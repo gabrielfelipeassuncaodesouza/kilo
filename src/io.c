9ccd0ea17e0bf36ef79a276f9d73f9866eb46c6c
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-12 18:22:01 -0300

    finish searching feature

--- src/io.c
+++ src/io.c
@@ -140,16 +140,39 @@ void editorSave(void) {
 }
 
 void editorFindCallback(char* query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   if(key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
   }
+  else if(key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  }
+  else if(key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  }
+  else {
+    last_match = -1;
+    direction = 1;
+  }
+
+  if(last_match == -1) direction = 1;
+  int current = last_match;
 
   for(int i = 0; i < E.numRows; i++) {
-    rowOfText* row = &E.rows[i];
+    current += direction;
+    if(current == -1) current = E.numRows - 1;
+    else if(current == E.numRows) current = 0;
+
+    rowOfText* row = &E.rows[current];
     char* match = strstr(row->render, query);
 
     if(match) {
-      E.cx = i;
+      last_match = current;
+      E.cx = current;
       E.cy = editorRowRxToCy(row, match - row->render);
       E.rowOffset = E.numRows;
       break;
@@ -163,7 +186,7 @@ void editorFind(void) {
   int saved_coloff = E.colOffset;
   int saved_rowoff = E.rowOffset;
 
-  char* query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char* query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
 
   if(query) free(query);
   else {
